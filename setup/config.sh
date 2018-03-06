09d1f732c9e1b466355c057ac29f58895715b22e
haam3r <andres@cert.ee>
2018-03-06 14:15:18 +0000

    add lab name

--- setup/config.sh
+++ setup/config.sh
@@ -6,7 +6,7 @@ export SERVERIPS="192.168.0.200/24 192.168.0.201/24"
 export ROUTERIP="192.168.0.254"
 
 # Lab name cannot contain any spaces
-export LAB_NAME="Example-LAB"
+export LAB_NAME="RFIDS-LAB1"
 
 counter=10
 
